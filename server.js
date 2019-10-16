e6ec4a26612a422d33098f59b18d32122fcd0657
Michael Ionov <imichaelionov@gmail.com>
2019-10-17 01:36:14 +0300

    fix server.js

--- server.js
+++ server.js
@@ -22,14 +22,14 @@ app.use('/api/clients', clients);
 app.use('/api/stores', stores);
 app.use('/api/products', products);
 
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+// Set static folder
+app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+// }
 
 const PORT = process.env.PORT || 5000;
 
